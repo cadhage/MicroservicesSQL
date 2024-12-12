d4be12ca74f21707078b8d154973563b0e25db4c
Chandrashekhar Adhage <cadhage@gmail.com>
2024-12-13 02:45:09 +0530

    Delete Microservice1/Microservice1 directory

--- Microservice1/Microservice1/src/main/java/org/microservice1/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.microservice1.Repository;
-
-import org.microservice1.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
